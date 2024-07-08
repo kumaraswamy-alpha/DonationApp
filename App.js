0f2816e6326facbcae8dbdfabd07d39234d42bf8
kumaraswamy-alpha <kumaraswamy.malga@accelyzei.com>
2024-07-08 16:13:02 +0530

    redux and redux tool kit

--- App.js
+++ App.js
@@ -2,12 +2,20 @@ import {View, Text} from 'react-native';
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import MainNavigation from './navigation/MainNavigation';
+import {Provider} from 'react-redux';
+import store from './redux/store';
+import {PersistGate} from 'redux-persist/integration/react';
+import {persistor} from './redux/store';
 
 const App = () => {
   return (
-    <NavigationContainer>
-      <MainNavigation />
-    </NavigationContainer>
+    <Provider store={store}>
+      <PersistGate persistor={persistor} loading={null}>
+        <NavigationContainer>
+          <MainNavigation />
+        </NavigationContainer>
+      </PersistGate>
+    </Provider>
   );
 };
 
--- screens/Home/Home.js
+++ screens/Home/Home.js
@@ -1,4 +1,4 @@
-import {SafeAreaView, View} from 'react-native';
+import {SafeAreaView, View, Pressable, Text} from 'react-native';
 import React from 'react';
 import globalStyle from '../../assets/styles/globalStyle';
 import Header from '../../components/Header/Header';
@@ -9,8 +9,15 @@ import {faSearch} from '@fortawesome/free-solid-svg-icons';
 import Search from '../../components/Search/Search';
 import SingleDonationItem from '../../components/SingleDonationItem/SingleDonationItem';
 import {horizontalScale} from '../../assets/styles/scaling';
+// Importing the useSelector hook from the React Redux library
+// This hook allows us to select and retrieve data from the store
+import {useSelector, useDispatch} from 'react-redux';
+import {updateFirstName} from '../../redux/reducers/User';
 
 const Home = () => {
+  const user = useSelector(state => state.user);
+  const dispatch = useDispatch();
+  console.log(user);
   return (
     <SafeAreaView style={[globalStyle.backgroudWhite, globalStyle.flex]}>
       {/* <Header title={'Azzahri A.'} type={1} />
@@ -23,7 +30,7 @@ const Home = () => {
           console.log(value);
         }}
       /> */}
-      <View
+      {/* <View
         style={{
           flexDirection: 'row',
           justifyContent: 'space-between',
@@ -45,7 +52,11 @@ const Home = () => {
           donationTitle={'Tree Cactus'}
           price={44}
         />
-      </View>
+      </View> */}
+      <Header title={user.firstName + ' ' + user.lastName} />
+      <Pressable onPress={() => dispatch(updateFirstName({firstName: 'N'}))}>
+        <Text>Press me to change first name</Text>
+      </Pressable>
     </SafeAreaView>
   );
 };
